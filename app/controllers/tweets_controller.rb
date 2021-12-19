4d8bc3087e05c07040e6117b6963570014a316d8
EC2 Default User <ec2-user@ip-172-31-25-204.us-east-2.compute.internal>
2021-12-19 08:37:41 +0000

    ”新規登録ページ作成”

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,5 @@
+class TweetsController < ApplicationController
+    def new
+        puts(11111111111111111)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'spreadsheets/read'
   get 'calendars/read'
   get 'static_pages/top'
+  get '/tweets', to:'tweets#new', as: 'new_tweet'
+ # この行を追加
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     # root 'application#cafe'
     root 'static_pages#top'
